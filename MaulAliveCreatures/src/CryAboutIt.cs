34576387545ee014f33bf2bdc0ff9be35202788b
BensoneWhite <75563186+Marmol-Salazar6969@users.noreply.github.com>
2023-04-09 22:22:16 -0600

    Add files via upload

---
+++ MaulAliveCreatures/src/CryAboutIt.cs
@@ -0,0 +1,48 @@
+﻿using BepInEx;
+using System.Security.Permissions;
+using System.Security;
+using System;
+using UnityEngine;
+using System.Linq;
+using System.Reflection;
+using MonoMod.RuntimeDetour;
+using MonoMod.Cil;
+using Mono.Cecil.Cil;
+using MoreSlugcats;
+using System.Collections.Generic;
+using Random = UnityEngine.Random;
+using UnityEngine.Rendering;
+using System.Xml;
+using SlugBase.Features;
+using static SlugBase.Features.FeatureTypes;
+
+namespace MaulAliveCreatures
+{
+    public static class CryAboutIt
+    {
+        public static void RegisterValues()
+        {
+            scavcry = new SoundID("scavcry", true);
+            munch = new SoundID("munch", true);
+            yummers = new SoundID("yummers", true);
+            
+        }
+        public static void UnregisterValues()
+        {
+            Unregister(scavcry);
+            Unregister(munch);
+            Unregister(yummers);
+            
+        }
+
+        private static void Unregister<T>(ExtEnum<T> extEnum) where T : ExtEnum<T>
+        {
+            extEnum?.Unregister();
+        }
+
+        public static SoundID scavcry;
+        public static SoundID munch;
+        public static SoundID yummers;
+        
+    }
+}
---
+++ MaulAliveCreatures/src/GlobalSuppressions.cs
@@ -0,0 +1,11 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Critical Code Smell", "S2696:Instance members should not write to \"static\" fields", Justification = "<Pending>", Scope = "member", Target = "~M:MaulAliveCreatures.MaulAliveCreatures.Player_GrabUpdate(On.Player.orig_GrabUpdate,Player,System.Boolean)")]
+[assembly: SuppressMessage("Critical Code Smell", "S2223:Non-constant static fields should not be visible", Justification = "<Pending>", Scope = "member", Target = "~F:MaulAliveCreatures.CryAboutIt.yummers")]
+[assembly: SuppressMessage("Critical Code Smell", "S2223:Non-constant static fields should not be visible", Justification = "<Pending>", Scope = "member", Target = "~F:MaulAliveCreatures.CryAboutIt.scavcry")]
+[assembly: SuppressMessage("Critical Code Smell", "S2223:Non-constant static fields should not be visible", Justification = "<Pending>", Scope = "member", Target = "~F:MaulAliveCreatures.CryAboutIt.munch")]
